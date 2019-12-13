36ac71b942960a6448c22b00a00f4049571d1a4a
Elias youssef <d-s-a00@hotmail.com>
2019-12-13 23:02:25 +0100

    streamlined configs

--- NotifyToAction/Configs.cs
+++ NotifyToAction/Configs.cs
@@ -8,20 +8,38 @@ using System.Threading.Tasks;
 
 namespace NotifyToAction
 {
+    class Configs : ConfigsTools
+    {
+        public Configs()
+        {
+            ConfigsFileName = "Configs.json";
+            GetFile();
+        }
+
+        public Configs(string fileName)
+        {
+            ConfigsFileName = fileName;
+            GetFile();
+        }
+
+        [JsonProperty("test", Required = Required.AllowNull)]
+        public string Test;
+    }
+
     /// <summary>
     /// Represents the base object for configurations object. Will mainly contain a save method to save inhereting objects as json.
     /// </summary>
-    class Configs
+    class ConfigsTools
     {
-        protected string FileName;
+        protected string ConfigsFileName;
 
         public void Save()
         {
             JsonSerializer serializer = new JsonSerializer();
             serializer.Formatting = Formatting.Indented;
-            ConfigFile configFile = (ConfigFile)this;
+            Configs configFile = (Configs)this;
             LocalAppDataFolder localAppDataFolder = new LocalAppDataFolder();
-            using (FileStream stream = localAppDataFolder.GetFile(FileName))
+            using (FileStream stream = localAppDataFolder.GetFile(ConfigsFileName))
             using (StreamWriter sw = new StreamWriter(stream))
             using (JsonWriter writer = new JsonTextWriter(sw))
             {
@@ -29,15 +47,15 @@ namespace NotifyToAction
             }
         }
 
-        public ConfigFile GetFile()
+        public Configs GetFile()
         {
             JsonSerializer serializer = new JsonSerializer();
             LocalAppDataFolder localAppDataFolder = new LocalAppDataFolder();
-            using (FileStream stream = localAppDataFolder.GetFile(FileName))
+            using (FileStream stream = localAppDataFolder.GetFile(ConfigsFileName))
             using (StreamReader sw = new StreamReader(stream))
             using (JsonReader reader = new JsonTextReader(sw))
             {
-                return serializer.Deserialize<ConfigFile>(reader);
+                return serializer.Deserialize<Configs>(reader);
             }
         }
     }
