1b0245654720d9fc780ce32d073fc5e09cbbe3bd
Elias youssef <d-s-a00@hotmail.com>
2019-12-15 00:52:53 +0100

    fixed configs

--- NotifyToAction/Configs.cs
+++ NotifyToAction/Configs.cs
@@ -13,14 +13,15 @@ namespace NotifyToAction
         public Configs()
         {
             ConfigsFileName = "Configs.json";
-            GetFile();
         }
 
         public Configs(string fileName)
         {
             ConfigsFileName = fileName;
-            GetFile();
         }
+
+        [JsonProperty("registeredSenderTriggers")]
+        public List<RegisteredSenderTriggers> RegisteredSenderTriggers;
 
         [JsonProperty("test", Required = Required.AllowNull)]
         public string Test;
@@ -33,11 +34,11 @@ namespace NotifyToAction
     {
         protected string ConfigsFileName;
 
-        public void Save()
+        public void Save<T>()
         {
             JsonSerializer serializer = new JsonSerializer();
             serializer.Formatting = Formatting.Indented;
-            Configs configFile = (Configs)this;
+            var configFile = this;
             LocalAppDataFolder localAppDataFolder = new LocalAppDataFolder();
             using (FileStream stream = localAppDataFolder.GetFile(ConfigsFileName))
             using (StreamWriter sw = new StreamWriter(stream))
@@ -47,7 +48,7 @@ namespace NotifyToAction
             }
         }
 
-        public Configs GetFile()
+        protected T GetFile<T>()
         {
             JsonSerializer serializer = new JsonSerializer();
             LocalAppDataFolder localAppDataFolder = new LocalAppDataFolder();
@@ -55,8 +56,42 @@ namespace NotifyToAction
             using (StreamReader sw = new StreamReader(stream))
             using (JsonReader reader = new JsonTextReader(sw))
             {
-                return serializer.Deserialize<Configs>(reader);
+                return serializer.Deserialize<T>(reader);
             }
+        }
+    }
+
+    public class LocalAppDataFolder
+    {
+        public LocalAppDataFolder(string assemblyName)
+        {
+            AssemblyName = assemblyName;
+        }
+
+        public LocalAppDataFolder()
+        {
+            AssemblyName = System.Reflection.Assembly.GetExecutingAssembly().GetName().Name;
+        }
+
+        private static readonly string LocalAppDataFolderPath = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+        private string AssemblyName;
+        public string AssemblyFolderPath
+        {
+            get
+            {
+                string FolderPath = Path.Combine(LocalAppDataFolderPath, AssemblyName);
+                if (!Directory.Exists(FolderPath))
+                {
+                    Directory.CreateDirectory(FolderPath);
+                }
+                return FolderPath;
+            }
+        }
+
+        public FileStream GetFile(string fileName)
+        {
+            string filePath = Path.Combine(AssemblyFolderPath, fileName);
+            return File.Open(filePath, FileMode.OpenOrCreate);
         }
     }
 }
